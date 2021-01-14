12b3f0b9468ee6610579a3eb05763180a288cbf7
Arik Alexander <arik@cc.com>
2021-01-14 18:38:03 +0000

    reference fixes

--- cve/reference.py
+++ cve/reference.py
@@ -9,20 +9,23 @@ counter = 0
 res = {}
 files_found = 0
 
-def handle_patch(cve_id, patch):
+def handle_files(cve_id, files):
     global files_found
-    str_patch = str(patch, 'utf-8')
+    for file in files:
+        if cve_id not in res:
+            res[cve_id] = []
+        if file not in res[cve_id]:
+            print('>>>>>>>>>>>>>>' + ' id:' + cve_id + ' ' + file)
+            res[cve_id].append(file)
+            files_found += 1
+
+def handle_patch(cve_id, str_patch):
+    global files_found
     words = re.split('\s+', str_patch)
     for word in words:
-        if '.c' or '.h' in word:
+        if '.c' in word or '.h' in word:
             files = re.findall(r'(/[a-zA-Z0-9\/]*\w+\.[ch])[^a-z]', word)
-            for file in files:
-                if cve_id not in res:
-                    res[cve_id] = []
-                if file not in res[cve_id]:
-                    print('>>>>>>>>>>>>>>' + file)
-                    res[cve_id].append(file)
-                    files_found += 1
+            handle_files(cve_id, files)
 
 
 def handle_ref(cve_id, r):
@@ -41,14 +44,27 @@ def handle_ref(cve_id, r):
     if 'commit' not in url:
         return
     counter = counter + 1
-    print('----------------------' + cve_id + ': ' + str(counter) + '--------------------------------------------------------')
-    print(url)
+    #print('----------------------' + cve_id + ': ' + str(counter) + '--------------------------------------------------------')
+    #print(url)
     try:
       response = requests.get(url)
     except:
       print('Eeception URL:' + url)
       return
-    handle_patch(cve_id, response.content)
+    handle_patch(cve_id, str(response.content, 'utf-8'))
+
+def handle_description(cve_id, cve):
+    if 'description' not in cve:
+        return
+    desc = cve['description']
+    if 'description_data' not in desc:
+        return;
+    desc_data = desc['description_data']
+    for i in desc_data:
+        if 'value' not in i:
+            continue
+        handle_patch(cve_id, i['value'])
+
 
 def handle_feed(data):
   for item in data["CVE_Items"]:
@@ -57,10 +73,12 @@ def handle_feed(data):
     if 'cve' not in item:
         continue
     cve = item['cve']
+    cve_meta_data = cve['CVE_data_meta']
+    #print('============================================ : ' + cve_meta_data['ID'])
+    handle_description(cve_meta_data['ID'], cve)
     if 'references' not in cve:
         continue
     references = cve['references']
-    cve_meta_data = cve['CVE_data_meta']
     if 'reference_data' not in references:
         continue
     ref_data = references['reference_data']
@@ -71,7 +89,6 @@ f = open("cve_log.txt", "w")
 
 suffixes = [
     "2020",
-    "2019",
 ]
 
 for suffix in suffixes:
